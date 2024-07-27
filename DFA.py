870e7ca4d6d1396049440fe644ed9b33e7b4c23e
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2024-07-27 13:36:27 +0000

    DFA.py added

---
+++ DFA.py
@@ -0,0 +1,26 @@
+class DFA:
+    def __init__ (self, q, sigma, delta, q0, f):
+        self.q = q #Estados
+        self.sigma = sigma #Alfabeto
+        self.delta = delta #Transiçoes
+        self.q0 = q0 #Estado inicial
+        self.f = f #Estado Final
+
+    def accepts(self, entrada):
+        state = self.q0
+        for i in entrada:
+            state = self.delta[state][i]
+        return state in self.f
+
+
+""" 
+#Exemplos, carece de mais testes: 
+transitions = {0:{'0':0, '1':1},
+       1:{'0':2, '1':0},
+       2:{'0':1, '1':2}}
+
+b = DFA({0, 1, 2}, {'0', '1'}, transitions, 0, {0})
+
+print(b.accepts('1011101'))
+
+print(b.accepts('10111011')) """
