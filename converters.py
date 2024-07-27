0f0c076cda2ef2723b636c82403e5d854084113c
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2024-07-27 11:23:58 +0000

    made changes to the NFA class

---
+++ converters.py
@@ -0,0 +1,29 @@
+import NFA
+
+"""
+It receives as input a character that make part of a regular expression
+and it says if it is really an input symboll or just a character that
+makes part of the syntax of a regular expression, like "[", or "+"
+"""
+def isOperand(er_symboll):
+
+"""
+The three next functions express the Thompson's algorithm
+"""
+
+"""
+It receives a string that represents the regular expression,
+a reference for a NFA object and a reference to integer "stateCounter".
+The name of each state of the NFA is going to be a number and,
+since the number of states are unknown, it is needed to save
+the amount of states, so that we can name each state, that's why
+"stateCounter" is useful.
+"""
+def prepareList(er, resultingNFA, stateCounter):
+	lista_er = []
+	for i in range(len(er)):
+		if isOperand(er[i]) :
+			resultingNFA.addInputStrAlphabet(er[i])
+            resutingNFA
+		else :
+			lista_er.append(er[i])
