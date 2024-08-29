b1163225e43d86a5dfa289b02e8b9ff8f8c97e2d
Gustavo <reginaldo.gustavo2003@gmail.com>
2024-08-29 18:09:13 -0300

    Parser fixed

--- er_2_nfa.py
+++ er_2_nfa.py
@@ -1,72 +1,6 @@
 from automata_and_re import NFA
 from collections import deque
 
-class operator:
-	def __init__(self, operator):
-		if (operator == '|'):
-			self.symbol = '|'
-			self.priority = 3
-		elif (operator == '.'):
-			self.symbol = '.'
-			self.priority = 2
-		elif (operator == '*'):
-			self.symbol == '*'
-			self.priority = 1
-		elif (operator == '('):
-			self.symbol = '('
-			self.priority = 0
-		elif (operator == ')'):
-			self.symbol = ')'
-			self.priority = 0
-
-"""
-It receives as input a character that make part of a regular expression
-and it says if it is really an input symboll or just a character that
-makes part of the syntax of a regular expression, like "[", or "+"
-"""
-def isOperand(er_symboll):
-	lower_case_alphabet=[chr(i) for i in range(ord('a'), ord('z')+1)]
-	upper_case_alphabet=[chr(i) for i in range(ord('A'), ord('Z')+1)]
-	numbers = [chr(i) for i in range(0,10)]
-	other_operands = ['_','=','+','-','*',';']
-	all_operands = []
-	all_operands.extend(lower_case_alphabet)
-	all_operands.extend(upper_case_alphabet)
-	all_operands.extend(numbers)
-	all_operands.extend(other_operands)
-	if (er_symboll in all_operands):
-		return True
-	return False
-
-def shuntingYard(er_expression):
-	output_queue = []
-	operator_queue = deque() #creates list with stack methods
-	for i in range (len(er_expression)):
-		#if the character is an operand
-		if (isOperand(er_expression[i])):
-			output_queue.append(er_expression)
-		#if the character is other thing
-		else:
-			operatorObj = operator(er_expression[i])
-			while ((operator_queue) and (operator_queue[-1].symbol != '(') and (operator_queue[-1].priority >= operatorObj.priority)):
-				output_queue.append(operator_queue[-1].symbol)
-				operator_queue.pop()
-				operator_queue.append(operatorObj)
-			if operatorObj.symbol == '(':
-				operator_queue.append(operatorObj)
-			elif operatorObj.symbol == ')':
-				while (operator_queue) and (operator_queue[-1].symbol != '('):
-					output_queue.append(operator_queue[-1].symbol)
-					operator_queue.pop()
-					if (operator_queue[-1].symbol == "("):
-						#output_queue.append(operator_queue[-1].symbol) we don't do this
-						operator_queue.pop() #discard the left parenthesis
-		while operator_queue :
-			if (operator_queue.symbol != '('):
-				output_queue.append(operator_queue[-1].symbol)
-			operator_queue.pop()
-		return output_queue
-
 """
 The three next functions express the Thompson's algorithm
 """
@@ -79,54 +13,6 @@ since the number of states are unknown, it is needed to save
 the amount of states, so that we can name each state, that's why
 "stateCounter" is useful.
 """
-def charToNFA(char):
-	trans = {
-		0:{char:{1}},
-		1:{}
-	}
-	nfa = NFA.NFA({0,1},{char},trans,0,{1})
-	return nfa
-
-
-def isER(char):
-	op = ['*','.','|','(',')']
-	for i in op:
-		if char == i:
-			return False
-	return True
-
-def prepareList(list):
-	newlist = []
-	for i in list:
-		if isER(i):
-			newlist.append(charToNFA(i))
-		else:
-			newlist.append(i)
-	return newlist
-
-"""
-Recieves a stack and do one of the three Regular Expressions operations
-a = {
-	0:{'0':{1}, '1':{1}},
-	1:{'0':{0}, '1':{2}},
-	2:{'0':{1}, '1':{0}}
-}
-b = {
-	0:{'0':{2}, '1':{1}},
-	1:{'0':{1}, '1':{2}},
-	2:{'0':{1}, '1':{0}}
-}
-
-a.b = {
-	0:{'0':{1}, '1':{1}},
-	1:{'0':{0}, '1':{2}},
-	2:{'0':{1}, '1':{0}, '':{3}},
-	3:{'0':{5}, '1':{4}},
-	4:{'0':{4}, '1':{5}},
-	5:{'0':{4}, '1':{3}}
-}
-"""
-
 #Do a shift in targetAutomata states in transitions and add them to resultingAutomata
 def shiftStates(shift, resultingAutomata, targetAutomata):
 	for i in targetAutomata.delta:
@@ -148,8 +34,65 @@ def updateEndStates(shift,targetState,resultingAutomata,targetAutomata):
 			else:
 				resultingAutomata.delta[i+shift][''] = newset
 
+#Create a simple NFA that accepts 'char'
+def charToNFA(char):
+	trans = {
+		0:{char:{1}},
+		1:{}
+	}
+	nfa = NFA.NFA({0,1},{char},trans,0,{1})
+	return nfa
 
-#Reduce the stack applying one operation
+def isER(char):
+	op = ['?','.','|','(',')']
+	for i in op:
+		if char == i:
+			return False
+	return True
+
+def token(list):
+	if len(list) == 0:
+		return list[0]
+	else:
+		if list[0] == 'INT':
+			return list[0]
+		if list[0] == 'CONST':
+			return list[0]
+	
+
+#Make an union an save every er end state
+def nfaUnion(list):
+	nfa = NFA.NFA({0},set(),{0:{'':set()}},0, set())
+	count = 1
+
+	for i in range(len(list)):
+		shiftStates(count,nfa,list[i])
+		for j in list[i].f:
+			newSet = set()
+			newSet.add(j+count)
+			nfa.f |= newSet
+		
+
+		newSet = set()
+		newSet.add(count)
+		nfa.delta[0][''] |= newSet
+		nfa.sigma |= list[i].sigma
+
+		for j in list[i].f:
+			count += j + 1
+	
+	return nfa
+
+#Turn an er list into an automata and symblos list
+def prepareList(list):
+	newlist = []
+	for i in list:
+		if isER(i):
+			newlist.append(charToNFA(i))
+		else:
+			newlist.append(i)
+	return newlist
+
 def erToNFAs(stack):
 	symbol = stack.pop()
 	if(symbol == '.'):
@@ -196,7 +139,7 @@ def erToNFAs(stack):
 		updateEndStates(shift+1,newfinal,afn,afn2)
 
 		stack.append(afn)
-	elif(symbol == '*'):
+	elif(symbol == '?'):
 		afn1 = stack.pop()
 		newfinal = len(afn1.delta)+1
 		afn = NFA.NFA({0,newfinal},afn1.sigma,{},0,{newfinal})
@@ -222,4 +165,24 @@ def erToNFA(er):
 			stack.append(i)
 			erToNFAs(stack)
 			# stack.pop(-2) #Delete the remaining '('
-	return stack
+	return stack[-1]
+
+def assemble():
+	inter = 'in.t.'
+	stringer = 'st.r.i.n.g.'
+	eqer = '='
+	adder = '+'
+	multer = '*'
+	suber = '-'
+	semicoloner = ';'
+	ider = '_ab|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z||AB|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z||_ab|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z||AB|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z||01|2|3|4|5|6|7|8|9||?.'
+	numer = '01|2|3|4|5|6|7|8|9|01|2|3|4|5|6|7|8|9|?.'
+	conster = '\"ab|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|?AB|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|?.01|2|3|4|5|6|7|8|9|?. ?.<?.>?.*?.-?.+?.=?.;?._?.?.\".'
+	greaterer = '>'
+	lesser = '<'
+
+	listNFA = [erToNFA(inter),erToNFA(stringer),erToNFA(eqer),erToNFA(adder),erToNFA(multer),erToNFA(semicoloner),erToNFA(ider),erToNFA(numer),erToNFA(conster),erToNFA(suber),erToNFA(greaterer),erToNFA(lesser)]
+
+	nfa = nfaUnion(listNFA)
+
+	return nfa
--- lexicalAnalyserA.py
+++ lexicalAnalyserA.py
@@ -1,1 +1,60 @@
+from er_2_nfa import assemble
+from nfa_2_dfa import nfa_to_dfa
+
+endStatesMap = {
+	6:'INT',
+	18: 'STRING',
+	566: 'NUM',
+	488: 'ID',
+	856: 'CONST',
+	26: 'SEMICOLON',
+	22: 'SUM_OP',
+	20: 'EQ_OP',
+	24: 'MULT_OP',
+	858: 'SUB_OP',
+	860: 'GREATER_OP',
+	862: 'LESS_OP'
+}
+
+def lexicalAnalisys():
+    inputFile = open("input.txt","r")
+    outputFile = open("output.txt","w")
+    input = inputFile.readlines()
+    lines = [line.rstrip('\n') for line in input]
+    err = False
+    inputFile.close()
+
+    outputLines = list()
+    for i in lines:
+        entryes = i.split(' ')
+        output = list()
+        for j in entryes:
+            tokens = list()
+            if j != '':
+                endState = dfa.accepts(j)
+                for k in endState:
+                    if(k in endStatesMap.keys()):
+                        tokens.append(endStatesMap[k])
+
+                if len(i) != 0:
+                    if len(tokens) == 0 :
+                        err = True
+                    else:
+                        output.append(tokens[0])
+        outputLines.append(output)
+
+    if(err):
+        outputFile.write('ERRO')
+    else:
+        for i in outputLines:
+            line = i
+            line.append('\n')
+            for j in range(len(line) -1):
+                line[j] += ' '
+            outputFile.writelines(line)
+
+
+nfa = assemble()
+dfa = nfa_to_dfa(nfa)
+lexicalAnalisys()
 
